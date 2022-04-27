75b03409b76b93daff0547aaf4aacdb88adea168
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-04-27 17:30:16 -0400

    .env file and local host and fixed package.json

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,4 @@
-const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
