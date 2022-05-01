75511ba534801eb4a2a7d0396a8f33d7c3fa71fa
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-05-01 14:22:31 -0400

    added checkersboard & tiles to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,13 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import Homepage from './pages/homepage';
-import Login from './pages/login';
-import Game from './pages/gamepage';
-import Highscore from './pages/highscore';
-import Signup from './pages/signup';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import Checkersboard from './Components/Checkersboard';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Homepage />} exact />
-              <Route path="/login" element={<Login />} exact />
-              <Route path="/signup" element={<Signup />} exact />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+    <div><Checkersboard/></div>
+  )
+
+   
 }
 
 export default App;
---
+++ client/src/components/Tile/index.tsx
@@ -0,0 +1,22 @@
+import './tile.css';
+
+interface Props {
+  image?: string;
+  number: number;
+}
+
+export default function Tile({number, image}: Props) {
+  if (number % 2 ===0 ){
+    return (
+      <div className='tile black-tile'>
+        {image &&<div style={{backgroundImage: `url(${image})`}} className='checkerpiece'></div>}
+      </div>
+    );
+  } else {
+    return (
+      <div className='tile red-tile'>
+        {image &&<div style={{backgroundImage: `url(${image})`}} className='checkerpiece'></div>}
+      </div>
+    ); 
+  }
+}
