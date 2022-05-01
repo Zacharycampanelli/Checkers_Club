c7252effc438a9d06f50e62206ddd0064e5e30b0
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-05-01 19:47:00 -0400

    moved code from App.js to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,51 +9,58 @@ import Footer from './Components/Footer';
 
 import Homepage from './pages/homepage';
 import Login from './pages/login';
-import Gamepage from './pages/gamepage';
+import Game from './pages/gamepage';
+// import Highscore from './pages/highscore';
 import Signup from './pages/signup';
 
-// import { Box, Link, Image } from '@chakra-ui/react'
+ import { Box, Link, Image } from '@chakra-ui/react'
 // import { ExternalLinkIcon } from '@chakra-ui/icons'
 
-// import chessboardPattern from './assets/chessboard-pattern-light.png';  
+// import chessboardPattern from './assets/chessboard-pattern-light.png'
 // import "@fontsource/bungee"
 
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
+    <Box className="maincontainer" 
+    // backgroundImage={chessboardPattern}
+    >
+    <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
             <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
+              <Route path="/" element={<Homepage />}  />
+              {/* <Route path="/game" element={} exact/> */}
+              {/* <Route path="/chat" element={} exact/> */}
+              {/* <Route path="/scores" element={} exact/> */}
+              {/* <Route path="/howto" element={} exact/> */}
+              <Route path="/login" element={<Login />}  />
+              <Route path="/signup" element={<Signup />}  />
             </Routes>
-          </div>
-          <div id='app'>
-            <Gamepage/>
           </div>
           <Footer />
         </div>
       </Router>
+    </ApolloProvider></Box>
   )
 };
