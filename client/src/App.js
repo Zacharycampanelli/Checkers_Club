2173368d67d40eaed490f9638a5558c203192fea
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-05-01 14:25:04 -0400

    push app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import './App.css'
-import Checkersboard from './Components/Checkersboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Homepage from './pages/homepage';
+import Login from './pages/login';
+import Game from './pages/gamepage';
+import Highscore from './pages/highscore';
+import Signup from './pages/signup';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div><Checkersboard/></div>
-  )
-
-   
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Homepage />} exact />
+              <Route path="/login" element={<Login />} exact />
+              <Route path="/signup" element={<Signup />} exact />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
