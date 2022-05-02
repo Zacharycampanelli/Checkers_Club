4b84b980b80204772bd4e846b3abd56352113bc9
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-05-02 13:53:50 -0400

    all rules added to rules page

--- client/src/pages/howto.js
+++ client/src/pages/howto.js
@@ -4,9 +4,10 @@ import { Flex, Spacer, Box, Text } from '@chakra-ui/react';
 const HowTo = () => {
   return (
     <main>
-      <Flex>
+        <Flex direction="column">
+      <Flex direction="row">
         <Box>
-          <Text>Checkers Rules</Text>
+          <Text fontSize='6xl'>Checkers Rules</Text>
           <Text>
             **Game Pieces and Board** Checkers is a board game played between two people on an 8x8 checked board like
             the one shown below. Each player has 12 pieces that are like flat round disks that fit inside each of the
@@ -17,14 +18,14 @@ const HowTo = () => {
             <br />
             **Taking a Turn** Typically the darker color pieces moves first. Each player takes their turn by moving a
             piece. Pieces are always moved diagonally and can be moved in the following ways:
-            <ul>
+            <br />
               <li>Diagonally in the forward direction (towards the opponent) to the next dark square.</li>
               <li>
                 If there is one of the opponent's pieces next to a piece and an empty space on the other side, you jump
                 your opponent and remove their piece. You can do multiple jumps if they are lined up in the forward
                 direction. *** note: if you have a jump, you have no choice but to take it.
               </li>
-            </ul>
+       
             <br />
             King Pieces:The last row is called the king row. If you get a piece across the board to the opponent's king
             row, that piece becomes a king. Another piece is placed onto that piece so it is now two pieces high. King
@@ -34,10 +35,38 @@ const HowTo = () => {
             a tie.
           </Text>
         </Box>
-        <Spacer />
+        </Flex>
+        <br />
+        <Flex>
         <Box>
-          <p>hi</p>
+        <Text fontSize="3xl">
+        Checkers Strategy and Tips
+        </Text>
+        <Text>
+         
+                <li>Sacrifice 1 piece for 2: you can sometimes bait or force the opponent to take one of your pieces enabling you to then take 2 of their pieces.</li>
+                <li>Pieces on the sides are valuable because they can't be jumped.</li>
+                <li>Don't bunch all your pieces in the middle or you may not be able to move, and then you will lose.</li>
+                <li>Try to keep your pieces on the back row or king row for as long as possible, to keep the other player from gaining a king.</li>
+                <li>Plan ahead and try to look at every possible move before you take your turn.</li>
+                <li>Practice: if you play a lot against a lot of different players, you will get better.</li>
+        </Text>
         </Box>
+        </Flex>
+        <br />
+        <Flex>
+        <Box>
+        <Text fontSize='3xl'>
+                Looking to go Pro?
+            </Text>
+            <Text>
+                <li>In official games, five minutes are allowed for each move. In the case where there is only one possible jump available, the player has only one minute in which to make it.</li>
+                <li>If the player whose move it is touches a piece that can be played, the player must move that piece or forfeit the game.</li>
+                <li>If during 20 moves (10 by each player), only kings have changed position and there appear to be repeating movements, then a referee will step in to determine the winner according to who holds the strongest position.</li>
+                <li>It is prohibited to annoy or distract your opponent. This includes making signs or sounds, pointing or hovering over the board with your hands or other body parts, needlessly delaying your moves, blowing smoke in your opponent's face, or appearing at the table dressed in a provocative manner</li>
+            </Text>
+        </Box>
+      </Flex>
       </Flex>
     </main>
   );
