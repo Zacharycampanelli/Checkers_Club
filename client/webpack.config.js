00029b9707890e6e0abaf27e8b1b32ebd9d43bc7
Twigster <treshagaye@gmail.com>
2022-05-03 19:07:19 -0400

    cleaned up pwa

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,11 @@
-// Inside of webpack.config.js:
+
 const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
-  // Other webpack config...
   plugins: [
-    // Other plugins...
+  
     new InjectManifest({
-      // These are some common options, and not all are required.
-      // Consult the docs for more info.
-      exclude: [/.../, '...'],
-      maximumFileSizeToCacheInBytes: ...,
+
       swSrc: 'service-worker.js',
     }),
   ],
